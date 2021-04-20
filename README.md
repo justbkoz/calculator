3054cb75ea35c997e6566c6299aaffeea04f1a92
Daniel Kozminski <danielkozminski@hotmail.com>
2021-04-20 12:26:06 -0400

    final commit, before moving on with my Odin Project learning

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # calculator
-simple calculator 
+simple calculator (started Apr 2021)
 
-This is the last project in The Odin Project's founcations course.
+This is the last project in The Odin Project's foundations course.
 
 The goal is to make a functioning calculator using basic javascript, html, and css.
+
+
+
+Learning Outcome: (as of mid-Apr 2021)
+    Calculator is functioning! however I'm looking forward to learning how to write modular code.  Feels like I did way too much copy pasting, and that I could have done the work with way less effort.
+
+    I learned about javascript's limitations when dealing with math and remainders.  Although I do not believe I found a best practice on how to deal with those limitations. 
+
+    Much of the CSS is copy/pasted.  I understand basics, but doubt I could come up with most of it without google.
+
+If I spend more time with this project:
+    Need to test my keyboard support with a full keyboard
+
+    The display doesnt look correct with too many characters in it.
+
+    I'd like to get the CSS looking better
+
+    Add the ability to resize the whole calculator, and work on different size displays
+
+    Add the ability to have it in a pop-up custom size window
--- calculator.js
+++ calculator.js
@@ -1,4 +1,3 @@
-const buttons = document.getElementsByClassName('buttons')
 const nums = document.getElementsByClassName('nums')
 
 document.body.addEventListener('click', clicked);
@@ -8,14 +7,43 @@ let workingNum = '';
 let totalDisplayed = false;
 
 const doMath = {
-    '+': function (x, y) { return x + y },
-    '-': function (x, y) { return x - y },
-};
-// console.log(doMath['+'](1, 2))   <---how this func works!
+    '+': function (x, y) { return ((x * 10) + (y * 10)) / 10 },
+    '-': function (x, y) { return ((x * 10) - (y * 10)) / 10 },
+    '/': function (x, y) { return ((x * 10) / (y * 10)) },
+    '*': function (x, y) { return ((x * 10) * (y * 10)) / 100 },
+}; //The 10s are to help alleviate the lack of js precision 
 
+document.addEventListener('keypress', function (e) {
+    e.preventDefault();
+    console.log(e)
+    if (e.key == 'Enter') { document.getElementById('equals').click() }
+    if (e.key == '0') { document.getElementById('zero').click() }
+    if (e.key == '1') { document.getElementById('one').click() }
+    if (e.key == '2') { document.getElementById('two').click() }
+    if (e.key == '3') { document.getElementById('three').click() }
+    if (e.key == '4') { document.getElementById('four').click() }
+    if (e.key == '5') { document.getElementById('five').click() }
+    if (e.key == '6') { document.getElementById('six').click() }
+    if (e.key == '7') { document.getElementById('seven').click() }
+    if (e.key == '8') { document.getElementById('eight').click() }
+    if (e.key == '9') { document.getElementById('nine').click() }
+    if (e.key == '-') { document.getElementById('minus').click() }
+    if (e.key == '=') { document.getElementById('add').click() }
+    if (e.key == '.') { document.getElementById('decimal').click() }
+    //if (e.key == '') { document.getElementById('clear').click() }
+    //if (e.key == '') { document.getElementById('multiply').click() }
+    //if (e.key == '') { document.getElementById('divide').click() }
 
+});
 
 function clicked(e) {
+    if ((workingNum == 'Not a Number') || workingNum == 'Infinity') {
+        workingNum = ``;
+        document.getElementById('lowerDisplay').innerText = `${workingNum}`
+    }
+    if (e.target.value == 'zero' && workingNum == '0') {
+        return;
+    }
     if (e.target.className.match('nums')) {
         if (totalDisplayed) {
             workingNum = '';
@@ -28,27 +56,61 @@ function clicked(e) {
         }
     }
     else if (e.target.value == 'clear') {
-        workingNum = '';
-        document.getElementById('lowerDisplay').innerText = `${workingNum}`;
+        if (workingNum == '') {
+            oldWorkingNum = '';
+            document.getElementById('upperDisplay').innerText = `${oldWorkingNum}`;
+        } else {
+            workingNum = '';
+            document.getElementById('lowerDisplay').innerText = `${workingNum}`;
+        }
     }
     else if (e.target.value == 'posNeg') {
-        console.log(e.target.innerText)
-    }
-    else if (e.target.value == 'percent') {
-        console.log(e.target.innerText)
+        if (!workingNum == '') {
+            workingNum *= -1;
+            workingNum = workingNum.toString();
+            document.getElementById('lowerDisplay').innerText = `${workingNum}`;
+        }
     }
     else if (e.target.value == 'divide') {
-        console.log(e.target.innerText)
+        if (workingNum == '') {
+        } else if (oldWorkingNum == '') {
+            oldWorkingNum = `${workingNum} /`;
+            workingNum = '';
+            document.getElementById('upperDisplay').innerText = `${oldWorkingNum}`
+            document.getElementById('lowerDisplay').innerText = `${workingNum}`
+        } else {
+            oldWorkingNum = `${oldWorkingNum} ${workingNum}`
+            workingNum = '';
+            split = oldWorkingNum.split(' ');
+            let newNum = doMath[`${split[1]}`](parseFloat(split[0]), parseFloat(split[2]))
+            oldWorkingNum = `${newNum} /`;
+            document.getElementById('upperDisplay').innerText = `${oldWorkingNum}`
+            document.getElementById('lowerDisplay').innerText = `${workingNum}`
+        }
     }
     else if (e.target.value == 'multiply') {
-        console.log(e.target.innerText)
+        if (workingNum == '') {
+        } else if (oldWorkingNum == '') {
+            oldWorkingNum = `${workingNum} *`;
+            workingNum = '';
+            document.getElementById('upperDisplay').innerText = `${oldWorkingNum}`
+            document.getElementById('lowerDisplay').innerText = `${workingNum}`
+        } else {
+            oldWorkingNum = `${oldWorkingNum} ${workingNum}`
+            workingNum = '';
+            split = oldWorkingNum.split(' ');
+            let newNum = doMath[`${split[1]}`](parseFloat(split[0]), parseFloat(split[2]))
+            oldWorkingNum = `${newNum} *`;
+            document.getElementById('upperDisplay').innerText = `${oldWorkingNum}`
+            document.getElementById('lowerDisplay').innerText = `${workingNum}`
+        }
     }
     else if (e.target.value == 'minus') {
         if (workingNum == '') {
-            return;
         } else if (oldWorkingNum == '') {
             oldWorkingNum = `${workingNum} -`;
             workingNum = '';
+            document.getElementById('upperDisplay').innerText = `${oldWorkingNum}`
             document.getElementById('lowerDisplay').innerText = `${workingNum}`
         } else {
             oldWorkingNum = `${oldWorkingNum} ${workingNum} -`
@@ -56,44 +118,58 @@ function clicked(e) {
             split = oldWorkingNum.split(' ');
             let newNum = doMath[`${split[1]}`](parseFloat(split[0]), parseFloat(split[2]))
             oldWorkingNum = `${newNum} -`;
-            document.getElementById('lowerDisplay').innerText = `${Number(newNum.toFixed(5))}`
+            document.getElementById('upperDisplay').innerText = `${oldWorkingNum}`
+            document.getElementById('lowerDisplay').innerText = `${workingNum}`
         }
     }
     else if (e.target.value == 'add') {
         if (workingNum == '') {
-            return;
         } else if (oldWorkingNum == '') {
             oldWorkingNum = `${workingNum} +`;
             workingNum = '';
+            document.getElementById('upperDisplay').innerText = `${oldWorkingNum}`
+            document.getElementById('lowerDisplay').innerText = `${workingNum}`
         } else {
             oldWorkingNum = `${oldWorkingNum} ${workingNum}`
             workingNum = '';
             split = oldWorkingNum.split(' ');
             let newNum = doMath[`${split[1]}`](parseFloat(split[0]), parseFloat(split[2]))
             oldWorkingNum = `${newNum} +`;
-            document.getElementById('lowerDisplay').innerText = `${Number(newNum.toFixed(5))}`
+            document.getElementById('upperDisplay').innerText = `${oldWorkingNum}`
+            document.getElementById('lowerDisplay').innerText = `${workingNum}`
         }
     }
-    else if (e.target.value == 'decimal') { // need to limit this to one decimal
-        workingNum += e.target.innerText;
-        document.getElementById('lowerDisplay').innerText = `${workingNum}`
+    else if (e.target.value == 'decimal') {
+        if (!workingNum.includes('.')) {
+            workingNum += e.target.innerText;
+            document.getElementById('lowerDisplay').innerText = `${workingNum}`
+        }
     }
     else if (e.target.value == 'equals') {
         if (workingNum == '') {
-            return;
         } else if (oldWorkingNum == '') {
-            return;
         } else {
             totalDisplayed = true;
             oldWorkingNum = `${oldWorkingNum} ${workingNum} =`
             split = oldWorkingNum.split(' ');
             let newNum = doMath[`${split[1]}`](parseFloat(split[0]), parseFloat(split[2]))
             oldWorkingNum = ``;
-            document.getElementById('lowerDisplay').innerText = `${Number(newNum.toFixed(5))}`
-            workingNum = `${Number(newNum.toFixed(5))}`;
+            document.getElementById('upperDisplay').innerText = `${oldWorkingNum}`
+            document.getElementById('lowerDisplay').innerText = `${Number(newNum.toFixed(7))}`
+            workingNum = `${Number(newNum.toFixed(7))}`;
         }
     }
-    else console.log('dang it') //need to get rid of this...
-
-
+    if (workingNum == 'NaN') {
+        workingNum = `Not a Number`;
+        document.getElementById('lowerDisplay').innerText = `${workingNum}`
+    }
+    if (oldWorkingNum == '' && workingNum == '') {
+        document.getElementById('clear').innerHTML = 'ALL<br>CLEAR';
+    } else if (workingNum == '') {
+        document.getElementById('clear').innerHTML = 'ALL<br>CLEAR';
+    } else if (oldWorkingNum == '') {
+        document.getElementById('clear').innerHTML = 'ALL<br>CLEAR';
+    } else {
+        document.getElementById('clear').innerHTML = 'CLEAR';
+    }
 }
